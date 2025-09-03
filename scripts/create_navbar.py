43c7caf989e7d6388c5ea37076ff54cfdc3270e4
dylansdaniels <dylansdaniels@users.noreply.github.com>
2025-09-03 18:24:13 +0000

    deploy: 832650777a7f7c7a2ae9bb9c3681a00c8b6b6339

--- scripts/create_navbar.py
+++ scripts/create_navbar.py
@@ -1,69 +1,62 @@
 # %% ######################################################################
-import os
 import json
-import textwrap
+import os
 
 # %% ######################################################################
 
 
-def generate_navbar_html():
+def generate_navbar_html(dev_build=False):
     """Function to generate the navbar from the structure specified
-       in the index.json file"""
+    in the index.json file"""
 
-    base_indent = '\t'
-    indent = '\t\t'  # need to deprecate this but it's currently used later
+    indent = "\t\t"
 
-    # create mySidebar
-    # create the header "navbar header"
-    # using textwrap.dedent() to make is easier to use html style in python
-    # without introducing extra indentation in the final html
-    html = textwrap.dedent("""
-        <div id="mySidebar" class="sidebar">
-            <div class="sidebar-close">
-            <svg class="popup-symbol" viewBox="0 0 24 24">
-            <use href="#popup-symbol" />
-            </svg>
-            </div>
-            <div class="navbar-header">
-                <div class="title-row">
-                    <a>
-                        Human Neocortical Neurosolver
-                    </a>
-                    <br>
-                        <svg class="collapse-icon" viewBox="0 0 16 16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16">
-                            <path d="M9 9H4v1h5V9z"/>
-                            <path fill-rule="evenodd" clip-rule="evenodd" d="M5 3l1-1h7l1 1v7l-1 1h-2v2l-1 1H3l-1-1V6l1-1h2V3zm1 2h4l1 1v4h2V3H6v2zm4 1H3v7h7V6z"/>
-                        </svg>
-                </div>
-                <div class="install-row">
-                    <a class="download-icon-link">
-                        <svg class="download-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M7 19L5.78311 18.9954C3.12231 18.8818 1 16.6888 1 14C1 11.3501 3.06139 9.18169 5.66806 9.01084C6.78942 6.64027 9.20316 5 12 5C15.5268 5 18.4445 7.60822 18.9293 11.001L19 11C21.2091 11 23 12.7909 23 15C23 17.1422 21.316 18.8911 19.1996 18.9951L17 19M12 10V18M12 18L15 15M12 18L9 15" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                        </svg>
-                    </a>
-                    <a>
-                        Installation
-                    </a>
-                </div>
-            </div>
-    """)
+    collapse_button = (
+        f"\n{indent}\t"
+        '<svg class="collapse-icon" '
+        'viewBox="0 0 16 16" '
+        'xmlns="http://www.w3.org/2000/svg" '
+        'viewBox="0 0 16 16"'
+        ">"
+        f"\n{indent}{indent}"
+        '<path d="M9 9H4v1h5V9z"/>'
+        f"\n{indent}{indent}"
+        '<path fill-rule="evenodd" '
+        'clip-rule="evenodd" '
+        'd="M5 3l1-1h7l1 1v7l-1 1h-2v2l-1 1H3l-1-1V6l1-1h2V3zm1 2h4l1 '
+        '1v4h2V3H6v2zm4 1H3v7h7V6z"/>'
+        f"\n{indent}\t</svg>"
+    )
+    html = (
+        '\t<div id="mySidebar" class="sidebar">'
+        + f'\n{indent}<div class="sidebar-close">'
+        + f'\n{indent}<svg class="popup-symbol" viewBox="0 0 24 24">'
+        + f'\n{indent}<use href="#popup-symbol" />'
+        + f"\n{indent}</svg>"
+        + f"\n{indent}</div>"
+        + f'\n{indent}<div class="navbar-header">'
+        + f"\n{indent}<a>"
+        + f"\n\t{indent}Human Neocortical Neurosolver"
+        + f"\n{indent}</a>\n{indent}<br>"
+        + collapse_button
+        + f"\n{indent}</div>"
+    )
 
-    html = textwrap.indent(html, base_indent)
-
-
-    # workshop_link = \
-    #     f'\n{indent}<a href="https://jonescompneurolab.github.io/textbook/tests/workshop.html">' + \
-    #     f'\n{indent}<div>' + \
-    #     f'\n{indent}\t<code class="workshop-button">Workshop Page</code>' + \
-    #     f'\n{indent}</div>' + \
-    #     f'\n{indent}</a>'
+    workshop_link = (  # noqa: F841 # this is to ignore the unused variable in ruff
+        f'\n{indent}<a href="https://jonescompneurolab.github.io/textbook/tests/"'
+        + 'workshop.html">'
+        + f"\n{indent}<div>"
+        + f'\n{indent}\t<code class="workshop-button">Workshop Page</code>'
+        + f"\n{indent}</div>"
+        + f"\n{indent}</a>"
+    )
 
     # html += workshop_link
 
     # load page index .json file
     index_path = os.getcwd() + "/index.json"
 
-    with open(index_path, 'r') as f:
+    with open(index_path, "r") as f:
         json_page_index = json.load(f)
 
     def get_absolute_paths(path=None):
@@ -81,31 +74,41 @@ def generate_navbar_html():
             else:
                 if not item == "README.md" and item.endswith(".md"):
                     # get the relative path for the web content only
-                    location = item_path.split(os.getcwd()+os.sep)[1]
+                    location = item_path.split(os.getcwd() + os.sep)[1]
                     location = location.split(item)[0]
 
                     page = item.split("_", 1)[1]
                     page = page.split(".md")[0] + ".html"
 
-                    md_pages[item] = '/textbook/' + location + page
+                    md_pages[item] = "/textbook/" + location + page
         return md_pages
 
-    def create_page_link(file, label, page_paths, indent):
+    def create_page_link(
+        file,
+        label,
+        page_paths,
+        indent,
+        dev_build=dev_build,
+    ):
         file_path = page_paths[file]
+        if dev_build:
+            file_path = file_path.replace('content', 'dev')
         return f'\n{indent}<a href="{file_path}">{label}</a>'
 
     def create_toggle_section(toggle_label):
-        section = f'\n{indent}<div class="sidebar-list">' + \
-                  f'\n{indent}\t<a id="sidebar-header"' + \
-                    ' onclick="toggleSubmenu(event)">' + \
-                  f'\n{indent}<span class="toggle-icon">+</span>' + \
-                  f'\n{indent}{indent}{toggle_label}' + \
-                  f'\n{indent}\t</a>' + \
-                  f'\n{indent}\t<div class="submenu">'
+        section = (
+            f'\n{indent}<div class="sidebar-list">'
+            + f'\n{indent}\t<a id="sidebar-header"'
+            + ' onclick="toggleSubmenu(event)">'
+            + f'\n{indent}<span class="toggle-icon">+</span>'
+            + f"\n{indent}{indent}{toggle_label}"
+            + f"\n{indent}\t</a>"
+            + f'\n{indent}\t<div class="submenu">'
+        )
         return section
 
     def build_navbar(json_page_index):
-        navbar_html = ''
+        navbar_html = ""
         page_paths = get_absolute_paths()
         ordered_links = []
         ordered_pages = []
@@ -132,21 +135,21 @@ def generate_navbar_html():
                         sub_page,
                         sub_name,
                         page_paths,
-                        indent+indent,
+                        indent + indent,
                     )
                     ordered_links.append(page_paths[sub_page])
                     ordered_pages.append(sub_name)
                 # Close toggle <div> sections
-                navbar_html += f'\n{indent}\t</div>'
-                navbar_html += f'\n{indent}</div>'
+                navbar_html += f"\n{indent}\t</div>"
+                navbar_html += f"\n{indent}</div>"
 
             # save ordered page links
             out_path = os.getcwd() + "/templates/ordered_page_links.json"
             ordered_page_links = {}
-            ordered_page_links['links'] = ordered_links
-            ordered_page_links['titles'] = ordered_pages
+            ordered_page_links["links"] = ordered_links
+            ordered_page_links["titles"] = ordered_pages
 
-            with open(out_path, 'w', encoding='utf-8') as f:
+            with open(out_path, "w", encoding="utf-8") as f:
                 json.dump(
                     ordered_page_links,
                     f,
@@ -159,7 +162,7 @@ def generate_navbar_html():
     navbar_html, ordered_links = build_navbar(json_page_index)
     html += navbar_html
     html += "\n\t<div style='height: 30px;'></div>"
-    html += '\n\t</div>'
+    html += "\n\t</div>"
     return html, ordered_links
 
 
